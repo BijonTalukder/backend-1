069d079bdda7bf3bd5784420691d37ca80a18ac8
BijonTalukder <bijontalukder1247@gmail.com>
2026-03-02 18:31:46 +0600

    a

--- src/server.ts
+++ src/server.ts
@@ -2,13 +2,13 @@ import app from './app';
 import config from './config/config';
 import connectDB from './config/db';
 import logger from './utils/Logger';
-import { seedDefaultCategories } from './utils/seedCategories';
 
 (async () => {
   try {
     await connectDB();
-    // await seedDefaultCategories(); // optional
+
     const PORT = config.port || 5000;
+
     app.listen(PORT, () => {
       logger.info(
         `🚀 Server running in ${process.env.NODE_ENV} mode on port ${PORT}`,
@@ -19,10 +19,3 @@ import { seedDefaultCategories } from './utils/seedCategories';
     process.exit(1);
   }
 })();
-// seedDefaultCategories();
-const PORT = config.port || 5000;
-app.listen(PORT, () => {
-  logger.info(
-    `🚀 Server running in ${process.env.NODE_ENV} mode on port ${PORT}`,
-  );
-});
