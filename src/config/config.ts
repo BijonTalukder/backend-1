7f963b8dd2dd3f058be4a20f8c6fc181f219a394
Bijon Talukder <bijontalukder1247@gmail.com>
2026-03-03 11:33:00 +0600

    update env

--- src/config/config.ts
+++ src/config/config.ts
@@ -12,7 +12,7 @@ interface Config {
 const config: Config = {
   port: Number(process.env.PORT) || 3000,
   nodeEnv: process.env.NODE_ENV || 'development',
-  mongoURI: 'mongodb+srv://BookAppsAdmin:0cc2Y35qpKHH2eTE@cluster0.ywgnkn8.mongodb.net/BookApps?retryWrites=true&w=majority',
+  mongoURI: process.env.MONGO_URI || '',
   jwtSecret: process.env.JWT_SECRET || 'secret',
 };
 
