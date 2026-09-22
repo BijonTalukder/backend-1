b0c950577b82a8c3b627d468bd20f4112428db5a
Bijon Talukder <81519797+BijonTalukder@users.noreply.github.com>
2026-09-22 21:23:30 +0600

    Fix CORS origin URL for hisabboi

--- src/app.ts
+++ src/app.ts
@@ -18,7 +18,7 @@ app.use(
     origin: [
       'https://cashbook-frontend-wine.vercel.app',
       'http://localhost:5173',
-      'hisabboi.vercel.app'
+      'https://hisabboi.vercel.app'
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
